503f376a27e86829c58292121f1522ab284ee85d
Trent Deal <trent.deal@gmail.com>
2022-07-21 09:51:24 -0400

    Add keymaps.lua to put non-plugin keymaps in

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,47 @@
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+local expr_opts = { noremap = true, expr = true, silent = true }
+
+-- Map leader key to SPC
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Unmap these for my sanity
+keymap('i', '<F1>', '<nop>', opts)
+keymap('n', '<F1>', '<nop>', opts)
+keymap('v', '<F1>', '<nop>', opts)
+keymap('n', 'Q', '<nop>', opts)
+keymap('n', 'q:', '<nop>', opts)
+
+-- Switch windows painlessly
+keymap('n', '<C-h>', '<C-w>h', opts)
+keymap('n', '<C-j>', '<C-w>j', opts)
+keymap('n', '<C-k>', '<C-w>k', opts)
+keymap('n', '<C-l>', '<C-w>l', opts)
+
+-- Windows splits
+keymap('n', '<leader>-', ':<C-u>split<cr>', opts)
+keymap('n', '<leader>\\', ':<C-u>vsplit<cr>', opts)
+
+-- Indent in visual mode and maintain cursor position
+keymap('v', '>', 'md>`d:delm d<cr>gv', opts)
+keymap('v', '<', 'md<`d:delm d<cr>gv', opts)
+
+-- Keep search matches in the middle of the window
+keymap('n', 'n', 'nzzzv', opts)
+keymap('n', 'N', 'Nzzzv', opts)
+
+-- Remove highlighting
+keymap('n', '\\', ':nohl<cr>', opts)
+
+--Remap for dealing with word wrap
+keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", expr_opts)
+keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", expr_opts)
+
+-- Y yank until the end of line
+keymap('n', 'Y', 'y$', opts)
+
+-- Remap escape to leave terminal mode
+keymap("t", "<ESC>", "<c-\\><c-n>", term_opts)
