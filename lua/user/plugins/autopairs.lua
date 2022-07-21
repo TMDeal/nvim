d8506330747f9ad63ca9b94bb7ee23968466d0eb
Trent Deal <trent.deal@gmail.com>
2022-07-21 18:57:38 -0400

    Add extra functionality when pressing space in pairs

--- lua/user/plugins/autopairs.lua
+++ lua/user/plugins/autopairs.lua
@@ -3,6 +3,9 @@ if not autopairs_ok then
     return
 end
 
+local Rule = require("nvim-autopairs.rule")
+local cond = require("nvim-autopairs.conds")
+
 autopairs.setup {
     -- Use treesitter
     check_ts = true,
@@ -25,6 +28,44 @@ autopairs.setup {
         highlight = 'Search',
         highlight_grey = 'Comment'
     },
+}
+
+
+-- Copied from: https://github.com/windwp/nvim-autopairs/wiki/Custom-rules
+-- shoul make it that when pressing <space> in between certain pairs, that it will add padding
+-- example:
+-- (|) -> ( | )
+autopairs.add_rules {
+  Rule(' ', ' ')
+    :with_pair(function(opts)
+      local pair = opts.line:sub(opts.col -1, opts.col)
+      return vim.tbl_contains({ '()', '{}', '[]' }, pair)
+    end)
+    :with_move(cond.none())
+    :with_cr(cond.none())
+    :with_del(function(opts)
+      local col = vim.api.nvim_win_get_cursor(0)[2]
+      local context = opts.line:sub(col - 1, col + 2)
+      return vim.tbl_contains({ '(  )', '{  }', '[  ]' }, context)
+    end),
+  Rule('', ' )')
+    :with_pair(cond.none())
+    :with_move(function(opts) return opts.char == ')' end)
+    :with_cr(cond.none())
+    :with_del(cond.none())
+    :use_key(')'),
+  Rule('', ' }')
+    :with_pair(cond.none())
+    :with_move(function(opts) return opts.char == '}' end)
+    :with_cr(cond.none())
+    :with_del(cond.none())
+    :use_key('}'),
+  Rule('', ' ]')
+    :with_pair(cond.none())
+    :with_move(function(opts) return opts.char == ']' end)
+    :with_cr(cond.none())
+    :with_del(cond.none())
+    :use_key(']'),
 }
 
 -- Integrate with nvim-cmp to add closing braces at end of function calls and stuff
