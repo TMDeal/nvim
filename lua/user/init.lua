086b133293b80329d00e58ade60d9a9f7987f70b
Trent Deal <trent.deal@gmail.com>
2022-07-22 15:36:21 -0400

    Make init.lua just load the user module

---
+++ lua/user/init.lua
@@ -0,0 +1,31 @@
+-- Needs to be loaded first
+require 'user.plugins.impaitent'
+
+-- Packer related stuff
+require 'user.packer'
+
+-- Sets vim options
+require 'user.options'
+
+-- Keymaps not related to plugins
+require 'user.keymaps'
+
+-- Autocmds not related to plugins
+require 'user.autocmds'
+
+-- configs for plugins
+require 'user.plugins.autopairs'
+require 'user.plugins.cmp'
+require 'user.plugins.comment'
+require 'user.plugins.lsp'
+require 'user.plugins.lualine'
+require 'user.plugins.markdown'
+require 'user.plugins.project'
+require 'user.plugins.telescope'
+require 'user.plugins.treesitter'
+require 'user.plugins.which-key'
+require 'user.plugins.nvim-tree'
+require 'user.plugins.bufferline'
+require 'user.plugins.toggleterm'
+require 'user.plugins.alpha'
+require 'user.plugins.tmux'
