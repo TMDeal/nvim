5e7b988b815c78f5bfb195f19735414a0ed46399
Trent Deal <trent.deal@gmail.com>
2022-07-22 10:55:15 -0400

    Add cmp-cmdline for cmp in the cmdline

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -74,3 +74,20 @@ cmp.setup {
         }
     }
 }
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'path' },
+        { name = 'cmdline', keyword_length = 3 }
+    }
+})
